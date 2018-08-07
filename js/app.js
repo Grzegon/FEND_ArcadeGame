d1c46350e2a5b3fae93c82a94ba46bea4d546191
Grzegon <grzegon.grzegonowaty@gmail.com>
2018-08-07 22:03:55 +0200

    Add game states handling

--- js/app.js
+++ js/app.js
@@ -1,46 +1,116 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(x, y, speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+
+        this.x += this.speed * dt;
+
+        // When off canvas, reset position of enemy to move across again
+        if (this.x > 550) {
+            this.x = -100;
+            this.speed = 100 + Math.floor(Math.random() * 512);
+        }
+    };
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/char-boy.png';
+    }
 
+    update() {
+        // Prevent player from moving beyond canvas wall boundaries
+        if (this.y > 380) {
+            this.y = 380;
+        }
+
+        if (this.x > 400) {
+            this.x = 400;
+        }
+
+        if (this.x < 0) {
+            this.x = 0;
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(keyPress) {
+        switch (keyPress) {
+            case 'left':
+                this.x -= this.speed + 50;
+                break;
+            case 'up':
+                this.y -= this.speed + 30;
+                break;
+            case 'right':
+                this.x += this.speed + 50;
+                break;
+            case 'down':
+                this.y += this.speed + 30;
+                break;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
 
+// Position "y" where the enemies will be created
+const enemyPosition = [60, 140, 220];
+const player = new Player(200, 380, 50);
+let enemy;
 
+enemyPosition.forEach(posY => {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+const input = event => {
+    {
+        const allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+        player.handleInput(allowedKeys[event.keyCode]);
+    }
+}
+document.addEventListener('keyup', input);
--- js/engine.js
+++ js/engine.js
@@ -13,16 +13,17 @@
  * writing app.js a little simpler to work with.
  */
 
-var Engine = (function(global) {
+var Engine = (function (global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
      */
-    var doc = global.document,
+    const doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime;
+        ctx = canvas.getContext('2d');
+    let lastTime;
+    let gameStatus = 'new game';
 
     canvas.width = 505;
     canvas.height = 606;
@@ -31,14 +32,14 @@ var Engine = (function(global) {
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
-    function main() {
+    main = () => {
         /* Get our time delta information which is required if your game
          * requires smooth animation. Because everyone's computer processes
          * instructions at different speeds we need a constant value that
          * would be the same for everyone (regardless of how fast their
          * computer is) - hurray time!
          */
-        var now = Date.now(),
+        const now = Date.now(),
             dt = (now - lastTime) / 1000.0;
 
         /* Call our update/render functions, pass along the time delta to
@@ -62,8 +63,7 @@ var Engine = (function(global) {
      * particularly setting the lastTime variable that is required for the
      * game loop.
      */
-    function init() {
-        reset();
+    init = () => {
         lastTime = Date.now();
         main();
     }
@@ -77,10 +77,72 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
+    update = (dt) => {
+        switch (gameStatus) {
+            case "new game": {
+                // Turn the keypress event listener in app.js off
+                document.removeEventListener("keyup", input);
+
+                const startInput = (event) => {
+                    const key = event.keyCode;
+
+                    if (key === 13) {
+                        gameStatus = "in game";
+                    }
+                };
+                document.addEventListener("keydown", startInput);
+                break;
+            }
+            // Here we do the "normal" things we'd do when the game is running, mainly updateEntities
+            case "in game": {
+                // Turn the keypress event listener in app.js back on
+                document.addEventListener('keyup', input);
+                // Call updateEntities to update each entity in the game
+                updateEntities(dt);
+                // Check collisions
+                checkCollisions();
+                break;
+            }
+            case "game over": {
+                // Turn the keypress event listener in app.js off
+                document.removeEventListener('keyup', input);
+
+                const gameoverInput = (event) => {
+                    const key = event.keyCode;
+                    if (key === 13) {
+                        gameStatus = "in game";
+                    }
+                };
+
+                document.addEventListener("keydown", gameoverInput);
+                break;
+            }
+        }
     }
+
+    /*
+     * Check for collision between player and enemies
+     */
+    checkCollisions = () => {
+        allEnemies.forEach(enemy => {
+            if (player.x < enemy.x + 60 &&
+                player.x + 37 > enemy.x &&
+                player.y < enemy.y + 25 &&
+                30 + player.y > enemy.y) {
+                player.x = 200;
+                player.y = 380;
+            }
+        });
+
+        // Check if player reached top of canvas and won the game
+        if (player.y < 0) {
+            player.x = 200;
+            player.y = 380;
+            // End game
+            gameStatus = 'game over';
+            this.init();
+        }
+    };
 
     /* This is called by the update function and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
@@ -89,8 +151,8 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
+    updateEntities = (dt) => {
+        allEnemies.forEach(function (enemy) {
             enemy.update(dt);
         });
         player.update();
@@ -102,54 +164,102 @@ var Engine = (function(global) {
      * they are flipbooks creating the illusion of animation but in reality
      * they are just drawing the entire screen over and over.
      */
-    function render() {
+    render = () => {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
+        const rowImages = [
+            'images/water-block.png',   // Top row is water
+            'images/stone-block.png',   // Row 1 of 3 of stone
+            'images/stone-block.png',   // Row 2 of 3 of stone
+            'images/stone-block.png',   // Row 3 of 3 of stone
+            'images/grass-block.png',   // Row 1 of 2 of grass
+            'images/grass-block.png'    // Row 2 of 2 of grass
+        ],
             numRows = 6,
-            numCols = 5,
-            row, col;
-        
-        // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+            numCols = 5;
+        let row, col;
 
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
+        // Before drawing, clear existing canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+        switch (gameStatus) {
+            case "new game": {
+                for (row = 0; row < numRows; row++) {
+                    for (col = 0; col < numCols; col++) {
+                        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                        // Text to display over the game board
+                        ctx.fillStyle = "black";
+                        ctx.font = "40px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("FEND Arcade Game", canvas.width / 2, canvas.height / 3);
+                        ctx.fillStyle = "black";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Press Enter To Start", canvas.width / 2, canvas.height / 2.5);
+                        ctx.fillStyle = "black";
+                        ctx.font = "16px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Use the arrow keys to move", canvas.width / 2, canvas.height / 1.7);
+                        ctx.fillText("Avoid the bugs to win the game!", canvas.width / 2, canvas.height / 1.5);
+                        ctx.fillStyle = "Black";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Good Luck!", canvas.width / 2, canvas.height / 1.2);
+                    }
+                }
+                break;
+            }
+            case "in game": {
+                /* Loop through the number of rows and columns we've defined above
+                 * and, using the rowImages array, draw the correct image for that
+                 * portion of the "grid"
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                for (row = 0; row < numRows; row++) {
+                    for (col = 0; col < numCols; col++) {
+                        /* The drawImage function of the canvas' context element
+                         * requires 3 parameters: the image to draw, the x coordinate
+                         * to start drawing and the y coordinate to start drawing.
+                         * We're using our Resources helpers to refer to our images
+                         * so that we get the benefits of caching these images, since
+                         * we're using them over and over.
+                         */
+                        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                    }
+                }
+                renderEntities();
+                break;
+            }
+            case "game over": {
+                for (row = 0; row < numRows; row++) {
+                    for (col = 0; col < numCols; col++) {
+                        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                        // Text to display over the game board
+                        ctx.fillStyle = "black";
+                        ctx.font = "40px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("CONGRATULATIONS!", canvas.width / 2, canvas.height / 3);
+                        ctx.fillText("You won!", canvas.width / 2, canvas.height / 2.3);
+                        ctx.fillStyle = "black";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Press Enter To Restart", canvas.width / 2, canvas.height / 2);
+                    }
+                }
+                break;
             }
         }
-
-        renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
      * tick. Its purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
-    function renderEntities() {
+    renderEntities = () => {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function (enemy) {
             enemy.render();
         });
 
@@ -160,8 +270,90 @@ var Engine = (function(global) {
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
-    function reset() {
-        // noop
+    reset = () => {
+        // Turn off key event listener needed to move character
+        document.removeEventListener('keyup', input);
+        const gameStartInput = (event) => {
+            const key = event.which || event.keyCode;
+            if (key === 13) {
+                gameStatus = "in game";
+            }
+        };
+        document.addEventListener("keydown", gameStartInput);
+
+        switch (gameStatus) {
+            case 'new game': {
+                // Display an empty game board with text here
+                const rowImages = [
+                    'images/water-block.png',   // Top row is water
+                    'images/stone-block.png',   // Row 1 of 3 of stone
+                    'images/stone-block.png',   // Row 2 of 3 of stone
+                    'images/stone-block.png',   // Row 3 of 3 of stone
+                    'images/grass-block.png',   // Row 1 of 2 of grass
+                    'images/grass-block.png'    // Row 2 of 2 of grass
+                ],
+                    numRows = 6,
+                    numCols = 5;
+                let row, col;
+
+                for (row = 0; row < numRows; row++) {
+                    for (col = 0; col < numCols; col++) {
+                        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                        // Text to display over the game board
+                        ctx.fillStyle = "white";
+                        ctx.font = "40px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Let's Play Frogger!", canvas.width / 2, canvas.height / 5.5);
+                        ctx.fillStyle = "white";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Press Enter To Start", canvas.width / 2, canvas.height / 4);
+                        ctx.fillStyle = "white";
+                        ctx.font = "16px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Use the arrow keys to move", canvas.width / 2, canvas.height / 3.3);
+                        ctx.fillText("Reach the water and collect gems to score", canvas.width / 2, canvas.height / 3.0);
+                        ctx.fillText("Difficulty increases when you reach water", canvas.width / 2, canvas.height / 2.75);
+                        ctx.fillText("Collect hearts for extra lives", canvas.width / 2, canvas.height / 2.55);
+                        ctx.fillText("Avoid the bugs to stay alive", canvas.width / 2, canvas.height / 2.37);
+                        ctx.fillStyle = "white";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Good Luck!", canvas.width / 2, canvas.height / 2.1);
+                    }
+                }
+                break;
+            }
+            case 'game over': {
+                // Display an empty game board with text here
+                rowImages = [
+                    'images/water-block.png',   // Top row is water
+                    'images/stone-block.png',   // Row 1 of 3 of stone
+                    'images/stone-block.png',   // Row 2 of 3 of stone
+                    'images/stone-block.png',   // Row 3 of 3 of stone
+                    'images/grass-block.png',   // Row 1 of 2 of grass
+                    'images/grass-block.png'    // Row 2 of 2 of grass
+                ],
+                    numRows = 6,
+                    numCols = 5;
+
+                for (row = 0; row < numRows; row++) {
+                    for (col = 0; col < numCols; col++) {
+                        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                        // Text to display over the game board
+                        ctx.fillStyle = "white";
+                        ctx.font = "40px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Game Over!", canvas.width / 2, canvas.height / 3);
+                        ctx.fillStyle = "white";
+                        ctx.font = "20px Comic Sans MS";
+                        ctx.textAlign = "center";
+                        ctx.fillText("Press Enter To Restart", canvas.width / 2, canvas.height / 2.6);
+                    }
+                }
+                break;
+            }
+        }
     }
 
     /* Go ahead and load all of the images we know we're going to need to
